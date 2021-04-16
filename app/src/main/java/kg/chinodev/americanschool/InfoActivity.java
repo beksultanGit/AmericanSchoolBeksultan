30b602e7dc820be430cda0303d26019f31345f3f
BeksultanGit <beksultan.asanakunov@bk.ru>
2021-04-16 22:02:14 +0600

    add x variable

--- app/src/main/java/kg/chinodev/americanschool/InfoActivity.java
+++ app/src/main/java/kg/chinodev/americanschool/InfoActivity.java
@@ -11,7 +11,7 @@ public class InfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
-
+        int x = 5;
 
     }
 }
